6b9664c0aa55303a328dfc9194c03a32401d1f16
vagrant <t.oleary@me.com>
2021-08-25 22:38:15 +0000

    add proper answer selection functionality

--- public/scripts/takeQuiz.js
+++ public/scripts/takeQuiz.js
@@ -8,10 +8,10 @@ $(() => {
     const stringQuizChildren = quizChildren.join("");
 
     const $form = `
-      <form class="new_quiz_submission" id="new_quiz_submission" action="" accept-charset="UTF-8" method="post">
+      <form action="" accept-charset="UTF-8">
           <input name="utf8" type="hidden" value="✓">
           <input type="hidden" name="authenticity_token" VALUE="">
-          <input type="hidden" value="14" name="quiz_submission[quiz_id]" id="quiz_submission_quiz_id">
+          <input type="hidden" value="14" >
           <p></p>
           <div id="quiz-container">${stringQuizChildren}
           </div>
@@ -37,32 +37,32 @@ $(() => {
         <div class="submission-answer">
           <h4 class="text-muted">Possible Answers</h4>
           <div class="list-group">
-            <input type="radio" name="quiz_submission[answers_attributes][0][option_id]" id="quiz_submission_answers_attributes_0_option_id_1642" value="1642">
-            <label for="quiz_submission_answers_attributes_0_option_id_1642">
+            <input type="radio" name=${question.id} id="o${question.option_a}q${question.id}" value=${question.option_a}>
+            <label for="o${question.option_a}q${question.id}">
               <div class="list-group-item" id="1642">
                 <p></p>
                 <p>${question.option_a}</p>
                 <p></p>
               </div>
             </label>
-            <input type="radio" name="" id="" value="">
-            <label for="">
+            <input type="radio" name=${question.id} id="o${question.option_b}q${question.id}" value=${question.option_b}>
+            <label for="o${question.option_b}q${question.id}">
               <div class="list-group-item" id="">
                 <p></p>
                 <p>${question.option_b}</p>
                 <p></p>
               </div>
             </label>
-            <input type="radio" name="" id="" value="">
-            <label for="">
+            <input type="radio" name=${question.id} id="o${question.option_c}q${question.id}" value="${question.option_c}">
+            <label for="o${question.option_c}q${question.id}">
               <div class="list-group-item" id="">
                 <p></p>
                 <p>${question.option_c}</p>
                 <p></p>
               </div>
             </label>
-            <input type="radio" name="" id="" value="">
-            <label for="">
+            <input type="radio" name=${question.id} id="o${question.option_d}q${question.id}" value="${question.option_d}">
+            <label for="o${question.option_d}q${question.id}">
               <div class="list-group-item" id="">
                 <p></p>
                 <p>${question.option_d}</p>
