20cff4b4fc0a093ffff73a2e959ee1b1d755bf67
Tzheng456 <Tzheng456@gmail.com>
2021-08-25 04:09:55 +0000

    debug showPubQuizzes script

--- public/scripts/showPubQuizzes.js
+++ public/scripts/showPubQuizzes.js
@@ -1,16 +1,23 @@
 $(() => {
-  $.get(`/api/`).then((pubQuizzes) => {
+  $.get("/api/").then((pubQuizzes) => {
     console.log(pubQuizzes);
+    const quizChildren = [];
+    for (const quiz of pubQuizzes) {
+      quizChildren.push(nextQuiz(quiz));
+    }
+    const stringQuizChildren = quizChildren.join("");
+    $(".spotlight-public-quizzes").append(stringQuizChildren);
   });
 
   const nextQuiz = (quiz) => {
     const $pubQuestion = `
-    <div class="row">
-    <div class="col-sm">
-    <p>${quiz.title}</p>
-    <button type="submit" href=${quiz.url} class="btn btn-outline-info btn-lg">Take this quiz</button>
-    </div>
-    </div>
-    `;
+      <div class="row">
+      <div class="col-sm">
+      <p>${quiz.title}</p>
+      <a href=${quiz.url} class="btn btn-outline-info btn-lg">Take this quiz</a>
+      </div>
+      </div>
+      `;
+    return $pubQuestion;
   };
 });
