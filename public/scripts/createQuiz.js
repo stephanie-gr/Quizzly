48d75a75bae012a59053e18644d096000983d0c4
Stephanie <stephanie.grant.2008@gmail.com>
2021-08-25 17:53:23 +0000

    create quiz edits

--- public/scripts/createQuiz.js
+++ public/scripts/createQuiz.js
@@ -1,15 +1,28 @@
 $(() =>  {
+
   //grab the form and save it to jquery obj
-  const $form = $(".quiz-title");
+  const $titleForm = $(".quiz-title");
 
-  //add event handler for $form, pass it callback
-  $form.on('submit', onSubmit)
+  //add event handler for $titleForm, pass it callback
+  $titleForm.on('submit', onTitleSubmit);
 
-  showURL();
+  //grab question form as jquery obj
+  const $questionForm =$('.new-question-form');
+
+  //questionForm has to listen for three event possibilities: the user uses the button to add a question, save the quiz as public, and save the quiz as private, respectively
+  $questionForm.on('submit', '#add-another-question', onQuestionSubmit);
+
+  // $questionForm.on('submit', '#save-public', onSavePublic);
+
+  // $questionForm.on('submit', '#save-private', onSavePrivate);
+
+
+  // showURL();
 
 });
 
-const onSubmit = function(event) {
+
+const onTitleSubmit = function(event) {
   event.preventDefault();
 
   //validation checks for no title input from user
@@ -22,72 +35,150 @@ const onSubmit = function(event) {
   }
 
   //hide title portion of form once entered
-  const $form = $(".quiz-title-form");
-  $form.slideUp();
+  const $titleForm = $(".quiz-title-form");
+  $titleForm.slideUp();
 
   //grab text from title
   const formData = $(this).serialize();
   console.log(formData);
 
-  $.post('/quizzes/new', formData)
-    .then(() => {
-      showURL();
-    })
+  const $questionForm = $(".question-and-answers");
+
+  return $questionForm.slideDown();
 }
 
-const showURL = function() {
-  $.ajax({
-    url: 'http://localhost:8080/quizzes/new',
-    method: 'GET',
-    dataType: 'json',
-    success: (tweetData) => {
-      renderTweets(tweetData);
-    },
-    error: (err) => {
-      console.error(err);
-    }
-  })
+const onQuestionSubmit = function(event) {
+  event.preventDefault();
+
+   //validation checks for no question input from user
+   const $noQuestionErrorMsg = $('#no-question-error');
+   $noQuestionErrorMsg.slideUp();
+
+   const questionBeforeSerialized = $("#question-text").val();
+   if (!questionBeforeSerialized) {
+     return $noQuestionErrorMsg.slideDown();
+   }
+
+   const $noAnswerErrorMsg = $('#no-answer-error');
+   $noAnswerErrorMsg.slideUp();
+
+   //right now, this only makes the user enter ONE option. if 0 options are entered, error msg will appear.
+   const optionBeforeSerialized = $("#option-a-text").val();
+   if (!optionBeforeSerialized) {
+     return $noAnswerErrorMsg.slideDown();
+   }
+
+   const questionText = $("question-text");
+   console.log(questionText.serialize());
+
+   const optionA = $("#option-a-text");
+   console.log(optionA.serialize());
+
+   const optionB = $("#option-b-text");
+   console.log(optionB.serialize());
+
+   const optionC = $("#option-c-text");
+   console.log(optionC.serialize());
+
+   const optionD = $("#option-d-text");
+   console.log(optionD.serialize());
+
+   //call something to SAVE this data to database and get another question form
+
 }
 
-const createTweetElement = function(tweetData) {
+// const onSavePublic = function(event) {
+//   event.preventDefault();
 
-  //variable to store the format of the new tweet, populated with values from tweetData
-  const $tweet = $(`
-  <article class="tweet" id="tweets-container">
-  <header>
-  <div>
-  <img src=${escape(tweetData.user.avatars)}>
-  <h3>${escape(tweetData.user.name)}</h3>
-  </div>
-  <h3>${escape(tweetData.user.handle)}</h3>
-  </header>
-  <p>${escape(tweetData.content.text)}</p>
-  <footer>
-  <p>${timeago.format(tweetData.user.created_at)}</p>
-  <span class="tweet-icons">
-  <span><i class="fas fa-flag"></i></span>
-  <span><i class="fas fa-retweet"></i></span>
-  <span><i class="fas fa-heart"></i></span>
-  </span>
-  </footer>
-  </article>`);
+//   //validation checks for no question input from user
+//   const $noQuestionErrorMsg = $('#no-question-error');
+//   $noQuestionErrorMsg.slideUp();
 
-  return $tweet; //return article
-}
+//   const questionBeforeSerialized = $("#question-text").val();
+//   if (!questionBeforeSerialized) {
+//     return $noQuestionErrorMsg.slideDown();
+//   }
 
-const renderTweets = function(tweets) {
+//   const $noAnswerErrorMsg = $('#no-answer-error');
+//   $noAnswerErrorMsg.slideUp();
 
-  const $tweetsContainer = $( '#tweets-container' );
-  $tweetsContainer.empty();
+//   //right now, this only makes the user enter ONE option. if 0 options are entered, error msg will appear.
+//   const optionBeforeSerialized = $("#option-a-text").val();
+//   if (!optionBeforeSerialized) {
+//     return $noAnswerErrorMsg.slideDown();
+//   }
 
-  for (const tweet of tweets) {
-    const $tweet = createTweetElement(tweet);
-    $tweetsContainer.prepend($tweet);
-  }
-}
+//   const questionText = $("question-text");
+//   console.log(questionText.serialize());
 
-const escape = function (str) {
-  let div = document.createElement("div");
-  div.appendChild(document.createTextNode(str));
-  return div.innerHTML;
-};
+//   const optionA = $("#option-a-text");
+//   console.log(optionA.serialize());
+
+//   const optionB = $("#option-b-text");
+//   console.log(optionB.serialize());
+
+//   const optionC = $("#option-c-text");
+//   console.log(optionC.serialize());
+
+//   const optionD = $("#option-d-text");
+//   console.log(optionD.serialize());
+
+//   //call something to SAVE this data as PUBLIC quiz and return  URLlink.slideDOwn()
+// }
+
+// const onSavePrivate = function(event) {
+//   event.preventDefault();
+
+//   //validation checks for no question input from user
+//   const $noQuestionErrorMsg = $('#no-question-error');
+//   $noQuestionErrorMsg.slideUp();
+
+//   const questionBeforeSerialized = $("#question-text").val();
+//   if (!questionBeforeSerialized) {
+//     return $noQuestionErrorMsg.slideDown();
+//   }
+
+//   const $noAnswerErrorMsg = $('#no-answer-error');
+//   $noAnswerErrorMsg.slideUp();
+
+//   //right now, this only makes the user enter ONE option. if 0 options are entered, error msg will appear.
+//   const optionBeforeSerialized = $("#option-a-text").val();
+//   if (!optionBeforeSerialized) {
+//     return $noAnswerErrorMsg.slideDown();
+//   }
+
+//   const questionText = $("question-text");
+//   console.log(questionText.serialize());
+
+//   const optionA = $("#option-a-text");
+//   console.log(optionA.serialize());
+
+//   const optionB = $("#option-b-text");
+//   console.log(optionB.serialize());
+
+//   const optionC = $("#option-c-text");
+//   console.log(optionC.serialize());
+
+//   const optionD = $("#option-d-text");
+//   console.log(optionD.serialize());
+
+//   //call something to SAVE this data as PRIVATE quiz and return  URLlink.slideDOwn()
+// }
+
+
+
+// const showURL = function() {
+//   $.ajax({
+//     url: 'http://localhost:8080/quizzes/new',
+//     method: 'GET',
+//     dataType: 'json',
+//     success: (quizData) => {
+//       renderTweets(quizData);
+//     },
+//     error: (err) => {
+//       console.error(err);
+//     }
+//   })
+// }
+
+
